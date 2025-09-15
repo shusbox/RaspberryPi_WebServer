1b7d6f931a391aba5987de861be4b1959958a474
shusbox <shusbox@naver.com>
2025-09-15 20:16:26 +0900

    고쳣당

--- ch02/index.py
+++ ch02/index.py
@@ -11,13 +11,13 @@ def home():
 @app.route("/submit", methods=["POST"])
 def save_db():
     data = request.get_json()
-    print(data.get("value"))
+    print(data)
     conn = pymysql.connect(host="localhost", user="root", password="q1w2e3", db="study")
     cur=conn.cursor()
     cur.execute(f"insert into numcount(num) values({data.get('value')})")
     conn.commit()
     conn.close()
-    return redirect(url_for("index"))
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
