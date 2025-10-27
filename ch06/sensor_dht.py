79d66a608487762e3457bd1142784a81a8252f01
allium0917 <sistus0917@gmail.com>
2025-10-27 19:27:40 +0900

    수정

--- ch06/sensor_dht.py
+++ ch06/sensor_dht.py
@@ -1,8 +1,15 @@
+import time
 import adafruit_dht
 import board
 
+sensor = adafruit_dht.DHT11(board.D18)  # 기존과 동일
+
 def get_now():
-    sensor = adafruit_dht.DHT11(board.D18)
-    temp = sensor.temperature
-    hum = sensor.humidity
-    return [temp, hum]
+    for _ in range(3):  # 최대 3회 재시도
+        try:
+            temp = sensor.temperature
+            hum = sensor.humidity
+            return temp, hum
+        except RuntimeError:
+            time.sleep(1.5)  # 센서 안정화 시간
+    return None, None  # 그래도 안되면 실패 처리
