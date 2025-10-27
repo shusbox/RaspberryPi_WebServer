a9c269e29b02ad223176c0ac88104f1f63a2a5ae
allium0917 <sistus0917@gmail.com>
2025-10-27 18:08:21 +0900

    ㅁ

--- ch06/index.py
+++ ch06/index.py
@@ -1,18 +1,31 @@
 from flask import Flask, request, render_template, jsonify
 import db_model
 import sensor_dht
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+    return render_template("index.html")
 
 @app.route('/api/now')
 def insertdb():
+    # DHT 센서에서 현재 데이터 읽기
     temp, hum = sensor_dht.get_now()
-    result = db_model.add(temp, hum)
-    return result
+
+    # DB에 삽입
+    db_model.add(temp, hum)
+
+    # 현재 시간 생성
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # JSON으로 응답
+    return jsonify({
+        "temperature": temp,
+        "humidity": hum,
+        "create_at": now
+    })
 
 @app.route('/api/record')
 def selectAll():
