bcc419e5d38d4ad2785c952c8e65fc8ed56e040c
allium0917 <sistus0917@gmail.com>
2025-10-27 17:36:18 +0900

    되나?

--- ch06/test.py
+++ ch06/test.py
@@ -1,14 +1,8 @@
 import adafruit_dht
 import board
-import time
 
-sensor = adafruit_dht.DHT11(board.D14, use_pulseio=False)
+sensor = adafruit_dht.DHT11(board.D14)
 
-while True:
-    try:
-        temp = sensor.temperature
-        hum = sensor.humidity
-        print([temp, hum])
-    except RuntimeError as e:
-        print(f"Read failed: {e}. Retrying...")
-    time.sleep(2)
+temp = sensor.temperature
+hum = sensor.humidity
+print([temp, hum])
--- ch06/test1.py
+++
@@ -1,8 +0,0 @@
-import adafruit_dht
-import board
-
-sensor = adafruit_dht.DHT11(board.D14)
-
-temp = sensor.temperature
-hum = sensor.humidity
-print([temp, hum])
