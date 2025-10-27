32d1fc570fbc30c9696ce903cd263c4b4cd49fff
shusbox <shusbox@naver.com>
2025-10-27 17:52:51 +0900

    js

--- ch06/static/index.js
+++ ch06/static/index.js
@@ -1,14 +1,32 @@
 const now = document.getElementById("now");
 const record = document.getElementById("record");
+const table = document.getElementById("table");
 
 now.addEventListener('click', () => {
   $.ajax({
     type: 'GET',
-    url: '/choiruru',
+    url: '/api/now',
     contentType: 'application/json'
-  }).done(() => {
+  }).done((result) => {
     console.log(result);
-  }).fail(() => {
+
+    const div = document.createElement("div");
+    div.className = "row";
+
+    const no = document.createElement("p");
+    const time = document.createElement("p");
+    const temperature = document.createElement("p");
+    const humidity = document.createElement("p");
+    no.textContent = "now";
+    time.textContent = "now";
+    temperature.textContent = result.temp;
+    humidity.textContent = result.hum;
+    
+    div.appendChild(no);
+    div.appendChild(time);
+    div.appendChild(temperature);
+    div.appendChild(humidity);
+  }).fail((result) => {
     console.log(result);
   });
 });
